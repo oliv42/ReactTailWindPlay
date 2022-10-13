6c9535c35c599a61e85677db107b841dc7f2bff9
Olivier.r <olivier.reboul@gmail.com>
2022-10-13 17:28:40 +0200

    le chien aboits tout le temps????

--- src/components/MyContacts.js
+++ src/components/MyContacts.js
@@ -7,10 +7,21 @@ export default function MyContacts() {
     const [search, setSearch] = useState("");
     const [user, setUser] = useState([]);
     const [isLoading, setIsloading] = useState(true);
+    const [resultSearch, setResultSearch] = useState([]);
     const handleChange = (e) => {
         setSearch(e.target.value);
     };
+    const filterUsers = () => {
+        const findUsers = user.filter((user) => {
+            return Object
+            .values(user)
+            .join(' ')
+            .includes(search.toLocaleLowerCase());
+        });
+        setResultSearch(findUsers)
+    };
     useEffect(() => {
+        console.log("On fletch");
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
             .then((json) => {
@@ -20,12 +31,20 @@ export default function MyContacts() {
             .catch((error) => console.error(error.message));
     }, []);
 
+    useEffect(() => {
+        if (search !== "") {
+            filterUsers();
+        } else {
+            setResultSearch([]);
+        }
+    }, [search]);
+
     useUpdateDocTitle(search);
 
     const msgDisplay = (msg, color) => {
         return <p style={{ textAlign: "center", color: color }}>{msg}</p>;
     };
-    console.log(user);
+
     return (
         <React.Fragment>
             {isLoading ? (
@@ -33,8 +52,16 @@ export default function MyContacts() {
             ) : (
                 <Search searchStr={search} searchHandler={handleChange} />
             )}
-            <hr/>
-            {<TableUsers dataArray={user}/>}
+            <hr />
+            {search !== "" ? (
+                resultSearch.length === 0 ? (
+                    msgDisplay("Aucun utilisateur correpondant", "blue")
+                ) : (
+                    <TableUsers dataArray={resultSearch} />
+                )
+            ) : (
+                <TableUsers dataArray={user} />
+            )}
         </React.Fragment>
     );
 }
